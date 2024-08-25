6ae5f4e7003c5ebcb4f0bff006af910607437927
irina211093 <irinadanilkor@gmail.com>
2024-08-25 20:04:22 +0200

    some changes in Main and in Elf class

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 import game.hero.Elf;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Elf elf1 = new Elf();
         elf1.setId(1);
         int elfId = elf1.getId();
         System.out.println("Id of Elf: " + elfId);
-
     }
 }
--- src/game/hero/Elf.java
+++ src/game/hero/Elf.java
@@ -5,27 +5,27 @@ public class Elf {
     private String name;
     private String gender;
 
-
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
-
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getGender() {
         return gender;
-
     }
+
     public void setGender(String gender) {
         this.gender = gender;
     }
-
-
 }
